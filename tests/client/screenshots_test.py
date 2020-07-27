0c10f0e6209ae7d1ddd1fa233f38f8b473b274b7
bodrovis <bodrovis@protonmail.com>
2020-07-27 12:50:46 +0300

    test screenshots with another project

--- tests/client/screenshots_test.py
+++ tests/client/screenshots_test.py
@@ -6,33 +6,33 @@ import pytest
 
 
 PROJECT_ID = "454087345e09f3e7e7eae3.57891254"
+SECOND_PROJECT_ID = "404021655ce68d0f36ad23.02802891"
 SCREENSHOT_ID = 343286
 
 @pytest.mark.vcr
 def test_screenshots(client):
     """Tests fetching of all screenshots
     """
-    screenshots = client.screenshots(PROJECT_ID)
-    assert screenshots.project_id == PROJECT_ID
-    assert screenshots.items[0].screenshot_id == SCREENSHOT_ID
+    screenshots = client.screenshots(SECOND_PROJECT_ID)
+    assert screenshots.project_id == SECOND_PROJECT_ID
+    assert screenshots.items[0].screenshot_id == 519938
 
 
 @pytest.mark.vcr
 def test_screenshots_pagination(client):
     """Tests fetching of all screenshots with pagination
     """
-    screenshots = client.screenshots(PROJECT_ID, {"page": 2, "limit": 3})
-    # Revise these tests after ITSD-870 is resolved:
-    assert screenshots.project_id == PROJECT_ID
+    screenshots = client.screenshots(SECOND_PROJECT_ID, {"page": 2, "limit": 1})
+    assert screenshots.project_id == SECOND_PROJECT_ID
     assert screenshots.current_page == 2
-    #assert screenshots.total_count == 26
-    #assert screenshots.page_count == 7
-    assert screenshots.limit == 3
+    assert screenshots.total_count == 2
+    assert screenshots.page_count == 2
+    assert screenshots.limit == 1
 
-    # assert not screenshots.is_last_page()
-    # assert not screenshots.is_first_page()
-    # assert screenshots.has_next_page()
-    # assert screenshots.has_prev_page()
+    assert screenshots.is_last_page()
+    assert not screenshots.is_first_page()
+    assert not screenshots.has_next_page()
+    assert screenshots.has_prev_page()
 
 
 @pytest.mark.vcr
